2f05d171c7e1ae320fbd3e71df27b8a90d20c768
Nivedita Setru <cceanse@live.ucl.ac.uk>
2014-09-29 17:15:33 +0100

    add new upgrade.php file

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,2 +1,47 @@
 <?php
 
+// This file keeps track of upgrades to
+// the chat module
+//
+// Sometimes, changes between versions involve
+// alterations to database structures and other
+// major things that may break installations.
+//
+// The upgrade function in this file will attempt
+// to perform all the necessary actions to upgrade
+// your older installation to the current version.
+//
+// If there's something it cannot do itself, it
+// will tell you what you need to do.
+//
+// The commands in here will all be database-neutral,
+// using the methods of database_manager class
+//
+// Please do not forget to use upgrade_set_timeout()
+// before any action that may take longer time to finish.
+
+/**
+ * 
+ * Upgrade 
+ *
+ * @package    local
+ * @subpackage testaccount_automation
+ * @copyright  2014 UCL
+ * @license    http://www.ucl.ac.uk
+ */
+
+
+/**
+ * 
+ * @global type $CFG
+ * @global type $DB
+ * @param type $oldversion
+ * @return boolean
+ */
+function xmldb_local_testaccount_automation_upgrade($oldversion) {
+    global $CFG, $DB;
+
+    $dbman = $DB->get_manager();
+
+    return true;
+}
